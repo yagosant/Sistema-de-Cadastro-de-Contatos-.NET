a7a8fa7d382b0cfd0954371bf93a28e9f941d76d
Yago Santos <yagojf27@gmail.com>
2024-01-08 14:55:14 -0300

    add paginação e filtro

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,7 +2,38 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // função pra fechar a msg
-
 //$('.close-alert').Click(function () {
   //  $(".alert").hide('hide');
 //});
+
+//usando os plugin data table
+let table = new DataTable('#table_contatos', {
+        "ordering": true,
+        "paging": true,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+            "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registros por página",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar: ",
+            "oPaginate": {
+                "sNext": "Proxímo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Último"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
+});
+
+
